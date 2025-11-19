5aeb54fb3aa6746fcc667abc6df73143fd0d646c
Tanu Shree <tanushree.wrk@gmail.com>
2025-11-19 21:19:56 +0530

    Merge develop into main (#1)
    * feat: daimo pay button
    
    * fix: handleQuantity function
    
    * fix: change regular to standard
    
    ---------
    
    Co-authored-by: Diksha <dikshas2911@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import BuyTickets from "./pages/BuyTickets";
+import { Providers } from "./providers";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/get-tickets" element={<BuyTickets />} />
-      </Routes>
-    </Router>
+    <Providers>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/get-tickets" element={<BuyTickets />} />
+        </Routes>
+      </Router>
+    </Providers>
   );
 }
 
